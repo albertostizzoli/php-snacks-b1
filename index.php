ebe00f4c39caa9982db9a9fc06387acf389a68d0
albertostizzoli <albertostizzoli@yahoo.it>
2023-11-28 16:26:12 +0100

    Complete snack 2

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+// SNACK 1
 $partite = [
     [
         'squadra_casa' => 'Olimpia Milano',
@@ -31,6 +32,12 @@ $partite = [
         'punti_ospite' => 98
     ],
 ];
+
+// SNACK 2
+$name = $_GET['name'];
+$mail = $_GET['mail'];
+$age = $_GET['age'];
+
 ?>
 
 
@@ -46,6 +53,7 @@ $partite = [
 </head>
 
 <body>
+    <!-- SNACK 1-->
     <div class="container-fluid text-center">
         <div class="row">
             <div class="col-12">
@@ -62,6 +70,33 @@ $partite = [
             </div>
         </div>
     </div>
+    <!-- SNACK 2-->
+    <div class="container-fluid text-center">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-bg-success">SNACK 2</h1>
+                <main>
+                    <form action="index.php" method="GET">
+                        <input type="text" name="name">
+                        <input type="mail" name="mail">
+                        <input type="text" name="age">
+                        <button class="btn btn-success">Invia</button>
+                    </form>
+                    <?php
+                    if (isset($_GET['name']) && isset($_GET['mail']) && isset($_GET['age'])) {
+                        if (strlen($name) > 3 && strpos($mail, '@') !== false && strpos($mail, '.') !== false && is_numeric($age)) { ?>
+                            <div class="text-success">
+                                Accesso riuscito
+                            </div>
+                        <?php } else { ?>
+                            <div class="text-danger">
+                                Accesso negato
+                            </div>
+                        <?php }
+                    } ?>
+                </main>
+            </div>
+        </div>
+    </div>
 </body>
-
 </html>
